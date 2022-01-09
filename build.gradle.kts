d1f0872a0fdc068228a7b2557405565f9a7ee4b9
a-ruskomaa <48881971+a-ruskomaa@users.noreply.github.com>
2022-01-09 18:27:28 +0200

    Add gradle application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
 	kotlin("plugin.jpa") version "1.6.10"
+	application
 }
 
 group = "com.example"
@@ -31,6 +32,10 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+application {
+	mainClass.set("com.example.bookcollection.BookCollectionApplicationKt")
 }
 
 allOpen {
